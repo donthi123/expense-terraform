6cfd268e83547e25c270674cb518b0951a404291
derangulasreevidhya <derangulasreevidhya@gmail.com>
2024-07-29 13:23:20 -0500

    adding token

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "vault" {
-  address = "https://vault-internal.donthi123.online"
+  address = "https://vault-internal.donthi123.online:8200"
   skip_tls_verify = true
   token = var.vault_token
 }
