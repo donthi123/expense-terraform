26bb111f90662b344027c173026c9bc79d79ce86
derangulasreevidhya <derangulasreevidhya@gmail.com>
2024-07-31 10:32:00 -0500

    update file names

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -20,7 +20,7 @@ resource "null_resource" "ansible" {
     inline = [
       "sudo pip3.11 install ansible",
       "ansible-pull -i localhost, -U https://github.com/donthi123/expense-ansible get.secrets.yml -e env=${var.env} -e role_name=${var.component} -e vault_token=${var.vault_token}",
-      "ansible-pull -i localhost, -U https://github.com/donthi123/expense-ansible expense.yml -e env=${var.env} -e role_name=${var.component} -e @common.json -e @${var.component}.json"
+      "ansible-pull -i localhost, -U https://github.com/donthi123/expense-ansible expense.yml -e env=${var.env} -e role_name=${var.component} -e @secrets.json -e @${var.component}.json"
     ]
   }
 
