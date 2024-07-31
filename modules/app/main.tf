b2aaaadfd88b76e1318a14dcaa12ba7fbb2ed3a5
derangulasreevidhya <derangulasreevidhya@gmail.com>
2024-07-31 10:41:21 -0500

    update file names

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -18,7 +18,7 @@ resource "null_resource" "ansible" {
     }
 
     inline = [
-      "sudo pip3.11 install ansible",
+      "sudo pip3.11 install ansible hvac",
       "ansible-pull -i localhost, -U https://github.com/donthi123/expense-ansible get.secrets.yml -e env=${var.env} -e role_name=${var.component} -e vault_token=${var.vault_token}",
       "ansible-pull -i localhost, -U https://github.com/donthi123/expense-ansible expense.yml -e env=${var.env} -e role_name=${var.component} -e @secrets.json -e @app.json"
     ]
