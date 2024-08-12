a8ea34b7350d9f4218bbf012cd8ced0828eb5f2b
derangulasreevidhya <derangulasreevidhya@gmail.com>
2024-08-12 12:44:12 -0500

    add route tables routes allowing communication with peering

--- main.tf
+++ main.tf
@@ -35,4 +35,5 @@ module "vpc" {
   subnet_cidr_block = var.subnet_cidr_block
   default_vpc_id = var.default_vpc_id
   default_vpc_cidr = var.default_vpc_cidr
+  default_route_table_id = var.default_route_table_id
 }
