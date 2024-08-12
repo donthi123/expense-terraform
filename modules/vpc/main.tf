4c7e67fe6d6c7ba7d06b82d86659515abecbd3a8
derangulasreevidhya <derangulasreevidhya@gmail.com>
2024-08-12 16:29:42 -0500

    adding internet gateway

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -71,3 +71,12 @@ resource "aws_subnet" "public" {
     Name = "${var.env}-public_subnet-${count.index+1}"
   }
 }
+
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.env}-igw"
+  }
+}
