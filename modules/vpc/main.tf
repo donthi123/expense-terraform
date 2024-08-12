8d17b361ecd103690113b66b6d63ac09bc477f58
derangulasreevidhya <derangulasreevidhya@gmail.com>
2024-08-12 17:22:20 -0500

    creating route tables

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -16,16 +16,12 @@ resource "aws_vpc_peering_connection" "main" {
    }
 }
 
-resource "aws_route" "main" {
-  route_table_id = aws_vpc.main.default_route_table_id
-  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
-  destination_cidr_block = var.default_vpc_cidr
-}
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
 
-resource "aws_route" "default-vpc" {
-  route_table_id = var.default_route_table_id
-  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
-  destination_cidr_block = var.vpc_cidr_block
+  tags = {
+    Name = "${var.env}-igw"
+  }
 }
 
 resource "aws_subnet" "frontend" {
@@ -38,7 +34,18 @@ resource "aws_subnet" "frontend" {
     Name = "${var.env}-frontend_subnet-${count.index+1}"
   }
 }
+resource "aws_route_table" "frontend" {
+  count = length(var.frontend_subnets)
+  vpc_id = aws_vpc.main.id
 
+  route {
+    cidr_block = var.default_vpc_cidr
+    vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+  }
+  tags = {
+    Name = "${var.env}-frontend-rt-${count.index+1}"
+  }
+}
 resource "aws_subnet" "backend" {
   count = length(var.backend_subnets)
   vpc_id = aws_vpc.main.id
@@ -49,6 +56,19 @@ resource "aws_subnet" "backend" {
     Name = "${var.env}-backend_subnet-${count.index+1}"
   }
 }
+resource "aws_route_table" "backend" {
+  count = length(var.backend_subnets)
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = var.default_vpc_cidr
+    vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+  }
+  tags = {
+    Name = "${var.env}-backend-rt-${count.index+1}"
+  }
+}
+
 
 resource "aws_subnet" "db" {
   count = length(var.db_subnets)
@@ -58,6 +78,18 @@ resource "aws_subnet" "db" {
 
   tags = {
     Name = "${var.env}-db_subnet-${count.index+1}"
+  }
+}
+resource "aws_route_table" "db" {
+  count = length(var.db_subnets)
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = var.default_vpc_cidr
+    vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+  }
+  tags = {
+    Name = "${var.env}-db-rt-${count.index+1}"
   }
 }
 
@@ -71,12 +103,21 @@ resource "aws_subnet" "public" {
     Name = "${var.env}-public_subnet-${count.index+1}"
   }
 }
-
-
-resource "aws_internet_gateway" "igw" {
+resource "aws_route_table" "public" {
+  count = length(var.public_subnets)
   vpc_id = aws_vpc.main.id
 
-  tags = {
-    Name = "${var.env}-igw"
+  route {
+    cidr_block = var.default_vpc_cidr
+    vpc_peering_connection_id = aws_vpc_peering_connection.main.id
   }
+  tags = {
+    Name = "${var.env}-public-rt-${count.index+1}"
+  }
+}
+
+resource "aws_route" "default-vpc" {
+  route_table_id = var.default_route_table_id
+  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+  destination_cidr_block = var.vpc_cidr_block
 }
