f4b5003a6deff3641bc46f4f2ed168eb13e2d348
derangulasreevidhya <derangulasreevidhya@gmail.com>
2024-08-12 14:19:23 -0500

    adding backend, db subnets

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -38,3 +38,25 @@ resource "aws_subnet" "frontend" {
     Name = "${var.env}-frontend_subnet-${count.index+1}"
   }
 }
+
+resource "aws_subnet" "backend" {
+  count = length(var.backend_subnets)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.backend_subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "${var.env}-backend_subnet-${count.index+1}"
+  }
+}
+
+resource "aws_subnet" "db" {
+  count = length(var.db_subnets)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.db_subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "${var.env}-db_subnet-${count.index+1}"
+  }
+}
