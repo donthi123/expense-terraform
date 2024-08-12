12e061727f2b860ecfe8b555b7dc1a9712f23118
derangulasreevidhya <derangulasreevidhya@gmail.com>
2024-08-12 10:58:34 -0500

    create vpc

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -14,18 +14,3 @@ resource "aws_subnet" "main" {
     Name = "${var.env}-subnet"
   }
 }
-# resource "aws_vpc_peering_connection" "main" {
-#    peer_vpc_id   = var.default_vpc_id
-#    vpc_id        = aws_vpc.main.id
-#    auto_accept    = true
-#
-#    tags = {
-#      Name = "${var.env}-vpc-to-default-vpc"
-#    }
-# }
-
-# resource "aws_route" "main" {
-#   route_table_id            = aws_vpc.main.default_route_table_id
-#   vpc_peering_connection_id = aws_vpc_peering_connection.main.id
-#   destination_cidr_block    = var.default_vpc_cidr
-# }
