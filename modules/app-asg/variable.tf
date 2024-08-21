a851b2343bce208960b0d9cae84b19e61a0e9708
derangulasreevidhya <derangulasreevidhya@gmail.com>
2024-08-21 14:09:52 -0500

    adding load balancers to auto scaling group

--- modules/app-asg/variable.tf
+++ modules/app-asg/variable.tf
@@ -9,3 +9,9 @@ variable "vpc_id" {}
 variable "server_app_port_sg_cidr" {}
 variable "bastion_nodes" {}
 variable "vault_token" {}
+variable "lb_type" {}
+variable "certificate_arn" {}
+variable "zone_id" {}
+variable "lb_subnets" {}
+variable "lb_app_port_sg_cidr" {}
+variable "lb_ports" {}
